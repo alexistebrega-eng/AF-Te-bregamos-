6750a4f76825fa5132a3dabf35a975f491a1f7f9
alexistebrega-eng <alexistebrega@gmail.com>
2026-04-03 23:45:16 +0200

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,8 +1,9 @@
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
+  apiKey: "AIzaSyARU1LNxaIB6htUvKSGR4JT5gp98hHR_JA",
+  authDomain: "alexis-te-brega.firebaseapp.com",
+  projectId: "alexis-te-brega",
+  storageBucket: "alexis-te-brega.firebasestorage.app",
+  messagingSenderId: "426334431059",
+  appId: "1:426334431059:web:dbfbc6f1bab2b00673a3a6",
+  measurementId: "G-JNPMBPHRGT"
 };
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
